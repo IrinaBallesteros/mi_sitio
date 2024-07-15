b1b72894de630b9f23ca2fef57327881f307be0b
Irina Ballesteros <ballesterosirina1@gmail.com>
2024-07-15 16:57:40 -0500

    actualizar pagina web

--- mi_aplicacion/src/index.js
+++ mi_aplicacion/src/index.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const router = express.Router();
+const { Pool } = require('pg');
 
+// Configura la conexión a la base de datos
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'bd_esfera_inteligente',
+  password: 'Exito2024$',
+  port: 5432, // Puerto por defecto de PostgreSQL
+});
 
+// Maneja la solicitud POST del formulario
+router.post('/contacto', async (req, res) => {
+  try {
+    const { nombre, telefono, correo, mensaje } = req.body;
+    await pool.query('INSERT INTO contactos (name, telefono, email, message) VALUES ($1, $2, $3, $4)', [name, telefono, email, message]);
+    res.redirect('/contacto'); // Redirige a la página principal
+  } catch (error) {
+    console.error('Error al insertar datos:', error);
+    res.status(500).send('Error al guardar los datos');
+  }
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+module.exports = router;
