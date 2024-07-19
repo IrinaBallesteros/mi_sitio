582431a25d84ed2e79d366ef3313b03dd802a712
Irina Ballesteros <ballesterosirina1@gmail.com>
2024-07-19 15:04:16 -0500

    actualizacion de sitio

--- mi_api/db/db.js
+++ mi_api/db/db.js
@@ -2,13 +2,6 @@ require('dotenv').config();
 
 const { Pool } = require('pg');
 
-// Verifica que las variables de entorno se carguen correctamente
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_DATABASE:', process.env.DB_DATABASE);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_PORT:', process.env.DB_PORT);
-
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
