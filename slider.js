0e0a5145c59bbfc327f69638b71d9039e130e232
Irina <iballesteroso@unicartagena.edu.co>
2025-05-24 17:41:49 -0500

    Actualización visual de mi sitio

--- slider.js
+++ slider.js
@@ -1,18 +1,15 @@
 $(document).ready(function(){
-    $('.slider').slick({
-        autoplay: true,
-        autoplaySpeed: 2000, // Cambiar la imagen cada 2 segundos
-        arrows: false, // Ocultar las flechas de navegación
-        dots: true, // Mostrar los puntos de navegación
-        adaptiveHeight: true,
-        responsive: [
-            {
-                breakpoint: 768, // Cambiar opciones para dispositivos con ancho de pantalla menor a 768px
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
+  $('.slider-wrapper').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    arrows: true,
+  });
 });
+
+  
+
+  
